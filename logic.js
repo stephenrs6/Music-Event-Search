b699d19fbe25da6f98821725588dd441520a99d0
Kyle <kyleconnolly1020@gmail.com>
2017-12-15 14:47:51 -0800

    added dynamically generated jQuery materialize

--- logic.js
+++ logic.js
@@ -10,8 +10,9 @@ var images = {};
 //geocoder variables
 var state;
 var geocoder;
+var eventLat = [];
+var eventLong = [];
 
-var maps = [];
 
 
 
@@ -45,7 +46,7 @@ function searchMusicGraph(input) {
             for (var i = 0; i < artists.length; i++) {
                 searchBandsInTown(artists[i]);
             }
-
+            
         });
 
     });
@@ -77,6 +78,7 @@ function searchBandsInTown(input) {
                 for (var a = 0; a < response.length; a++) {
                     if (response[a].venue.region === state) {
                         results.push(response[a]);
+                       
 
                     }
                 }
@@ -134,14 +136,128 @@ $('#search').keypress(function (e) {
         //Runt the Music Graph search for related artists array
         searchMusicGraph(artist);
         console.log(results);
-        createCarouselItem(results);
+        
     }
 });
 
+function parseEventResults (element) {
+    
+        eventLat.push(element.venue.latitude);
+        eventLong.push(element.venue.longitude);
 
-function createCarouselItem(input) {
-    for (i = 0; i < results.length; i++) {
+}
+
+function initMap(latitude, longitude, mapInput) {
+    var uluru = {lat: latitude, lng: longitude};
+    var map = new google.maps.Map(document.querySelector(mapInput), {
+      zoom: 15,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
+
+
+function carouselGenerator(event, imgs, eventlatitude, eventlongitude){
+    // DYNAMICALLY CREATING THE CAROUSEL
+    // create loop for every result to create a new card for the carousel
+    
+        // create <div class="carousel-item">
+        var item = $("<div>");
+        item.addClass('carousel-item');
+        // append to <div class="carousel">
+        $(".carousel").append(item);
+        // =============PARENT DIV========= 
+        // create <div class="card sticky-action">
+        var card = $('<div>');
+        card.addClass('card sticky-action');
+        // append to <div class="carousel-item">
+        $(".carousel-item").append(card);
+        // =========IMG DIV==============
+        // create <div class="card-image waves-effect waves-block">
+        var pic = $('<div>');
+        pic.addClass('card-image waves-effect waves-block');
+        // create img and link to url 
+        var photo = $("<img>");
+        photo.addClass('activator');
+        photo.attr('src', imgs);
+        // John's img code
+        // append <div class="card-image waves-effect waves-block">
+        $(pic).append(photo);
+        // append to <div class="card sticky-action">
+        $(card).append(pic);
+        // ========CARD CONTENT==========
+        //create <div class="card-content">
+        var content = $('<div>');
+        content.addClass("card-content");
+        // add span to the div
+        var cardTitle = $('<span>');
+        cardTitle.addClass('card-title activator grey-text text-darken-4');
+        //pull in name from bands in town
+        cardTitle.text(event.lineup[0]+ " Is Playing @ " + event.venue.name);
+        var vert = $("<i>more_vert</i>");
+        vert.addClass("material-icons right");
+        // append i to span
+        cardTitle.append(vert)
+        // append span to card-content
+        content.append(cardTitle);
+        // append to <div class="card sticky-action">
+        card.append(content);
+        // ==========CARD ACTION=========
+        // create <div class="card-action">
+        var action = $('<div>');
+        action.addClass("card-action");
+        // add <a href="#">Bandwebsite</a>
+        var website = $("<div>"); 
+        website.html("<a href='" + event.url + "'>BUY TICKETS</a>");
+        action.append(website);
+        // append to <div class="card sticky-action">
+        $(card).append(action);
+        // =========CARD REVEAL===========
+        // create <div class="card-action card-reveal"> 
+        var reveal = $('<div>');
+        reveal.addClass("card-action card-reveal");
+        var cardTitleTwo = $("<span>");
+        cardTitleTwo.addClass("card-title grey-text text-darken-4");
+        cardTitle.text(event.venue.name);
+        var closeSymbol = $("<i>close</i>");
+        closeSymbol.addClass("material-icons right");
+        cardTitleTwo.append(closeSymbol);
+        var mapview = $("<div>");
+        var mapid = 'map';
+        mapview.attr('id', mapid);
+        parseEventResults(event);
+        initMap(eventlatitude, eventlongitude, mapid);
+        cardTitleTwo.append(mapview);
+        reveal.append(cardTitleTwo);
+        $(card).append(reveal);
+        if($('.carousel').hasClass('initialized')){
+            $('.carousel').removeClass('initialized');
+        }
+        $('.carousel').carousel();
+    
+    
+    }
+    
+
+
+
+
+
+function createCarouselItem() {
+    console.log("test1");
+    for (var i = 0; i < results.length; i++) {
+        console.log("test2");
         var name = results[i].lineup[0].toLowerCase();
-        
+        carouselGenerator(results[i], images[name], eventLat[i], eventLong[i]);
     }
 }
+
+
+
+
+
+
+
